e7e946d92fdac63739d8487d4bb6c97825318c2c
Gianfranco <gianpa@gmail.com>
2021-12-05 14:24:53 +0000

    Add Sublime Merge Key Bindings (Keyboard shortcuts)

---
+++ setup-apps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+# Sublime Merge
+cp apps/sublime-merge/* $HOME/Library/Application\ Support/Sublime\ Merge/Packages/User/
+echo "Finishing setting up Sublime Merge"
+
+echo "Done!"
